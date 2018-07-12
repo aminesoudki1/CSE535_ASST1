2346cadd1e4ba7449e44ccbd327b2b847a7923d0
aminesoudki1 <40844220+aminesoudki1@users.noreply.github.com>
2018-07-12 14:00:26 -0700

    graph updating on run and clearing on stop

--- app/src/main/java/com/example/amine/cse535_asst1/MainActivity.java
+++ app/src/main/java/com/example/amine/cse535_asst1/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.amine.cse535_asst1;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.SearchManager;
+import android.content.DialogInterface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +18,7 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import butterknife.BindView;
@@ -50,17 +55,22 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.gv_graph)
     GraphView graph;
 
+    ArrayList<DataPoint> dataPoints;
+
     //random to generate the random data points
     final Random random = new Random();
     //x is the x axis here refers to time
-    int x = 0;
+    double x = 0;
     //handler to schedule an update every 1 second.
     final Handler handler = new Handler();
     //reference to the runnable that we are running every second
     Runnable run_every_interval;
     //if the heart rate monitor is running
     boolean running = false;
-
+    //update time
+    long time = 1000;
+    //number of points displayed on graph
+    int number_of_points = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,38 +78,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-
+        dataPoints = new ArrayList<>();
         graph.getViewport().setScalable(true); // enables horizontal zooming and scrolling
         graph.getViewport().setScalableY(true); // enables vertical zooming and scrolling
 
 
         graph.setTitle("HeartBeat Monitor");
-
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(20);
 
     }
-
     @OnClick(R.id.bt_run)
     public void run() {
+        if(et_age.getText().toString().isEmpty()) {
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Alert");
+            alertDialog.setMessage("Patient Information Not available");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
         if(!running) {
             Log.d(TAG, "button run");
             final LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
                     new DataPoint(x, 0),
 
             });
-            x = x + 1;
+            x = x + (double)(time)/1000;
             graph.addSeries(series);
             run_every_interval = new Runnable() {
                 @Override
                 public void run() {
 
-                    series.appendData(new DataPoint(x, random.nextDouble()), true, 50, false);
-                    x = x + 1;
-                    handler.postDelayed(this, 1000);
+                    DataPoint data = new DataPoint(x, random.nextDouble());
+                    dataPoints.add(data); //saving the data point in an array
+                    series.appendData(data, true, number_of_points, false);
+                    x = x + (double)time/1000.0;
+                    handler.postDelayed(this, time);
+
                 }
             };
-            //run after 1 sec
+            //run after time
             running = true;
-            handler.postDelayed(run_every_interval, 1000);
+            handler.postDelayed(run_every_interval, time);
         }
 
     }
@@ -108,7 +134,8 @@ public class MainActivity extends AppCompatActivity {
     public void stop() {
         Log.d(TAG,"button stop");
         x = 0;
-        graph.removeAllSeries();
+        graph.removeAllSeries(); //clear graph
+        dataPoints.clear(); //clear array
         if(run_every_interval !=null) {
             handler.removeCallbacks(run_every_interval);
             running = false;
